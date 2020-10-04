9847d2a9467b6fc64dc317af1140578e00d46475
Latricia <tricianickelberry@gmail.com>
2020-10-03 17:21:28 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -12,16 +12,26 @@ document.addEventListener("DOMContentLoaded",  makeFetch)
         
       })
       
-      .then(json => addImages(json))
+      .then(json => addImages(json.message))
   }
-  function addImages(json){ 
-      console.log(json.message)
+  function addImages(images){ 
+      console.log(images)
 
-      const html = json.message.map(breed =>{
+     let div = document.querySelector('#dog-image-container') //select container/parent from page
+      console.log("div", div)
+      const html = images.forEach(image =>{
+          console.log("image", image)
+
+          let img = document.createElement("img") // create child element
+          
+          img.src = image //updated child element
+          console.log(img)
+
+          div.appendChild(img) //append child to parent
 
       })
     // grab the dog image container and save it to a variable
-      document.querySelector('#dog-image-container').innerHtml = "<h1>HIIII</h1>"
+      
        // console.log(dog-image-container)
       
     //div id="dog-image-container"
@@ -32,3 +42,12 @@ document.addEventListener("DOMContentLoaded",  makeFetch)
     // 
 
   }
+
+  document.addEventListener("DOMContentLoaded",  nextFetch)
+
+  function nextFetch(){
+    fetch("https://dog.ceo/api/breeds/list/all")
+    .then(function(response){
+      return response.json();
+      
+  })
